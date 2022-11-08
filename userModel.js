2eb2097e073c27f3abfbd7ddf23db1115aae19ad
Dor Bezalel <dor.netcraft@gmail.com>
2022-11-08 17:05:37 +0200

    Add email validetor

--- userModel.js
+++ userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+// const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
 	name: {
@@ -9,6 +10,7 @@ const userSchema = new mongoose.Schema({
 		type: String,
 		unique: true,
 		required: [true, "Please provide your email!"],
+		// validate: [validator.isEmail, 'Please provide a valid email!'],
 	},
 	password: {
 		type: String,
